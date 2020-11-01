446fb89a380c7f05b4d6813f3636b7ffd6bbee90
Aaron Lathrop <alathrop78@gmail.com>
2020-11-01 09:52:44 -0700

    code clean createFolderStructure and add comments

--- createFolderStructure.js
+++ createFolderStructure.js
@@ -2,16 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 function createFolderStructure(templateDirectoryPath, destinationPath) {
+    // Add destination path if it doesn't exist already
     if (!fs.existsSync(path.join(process.cwd(), destinationPath))) {
         fs.mkdirSync(path.join(process.cwd(), destinationPath));
     }
+
+    // Recursively add all sub-folders
     const files = fs.readdirSync(templateDirectoryPath, { withFileTypes: true });
     files.forEach(dirnet => {
-        const newPath = path.join(process.cwd(), destinationPath, dirnet.name);
-        if (!fs.existsSync(newPath) && dirnet.isDirectory()) {
-            fs.mkdirSync(newPath);
-        }
         if (dirnet.isDirectory()) {
+            // Call method again but one-level deeper
             createFolderStructure(path.join(templateDirectoryPath, dirnet.name), path.join(destinationPath, dirnet.name));
         }
     });
