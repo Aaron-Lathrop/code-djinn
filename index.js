7c1f1dee857698f159f0c13278de0b046529ab8c
Aaron Lathrop <alathrop78@gmail.com>
2020-11-06 15:56:26 -0700

    organize /write-files into sub-folders by type of file created

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { colors, logWithColor } = require('./lib/utils/consoleUtils');
 const questionUtils = require('./lib/questions/questionUtils');
 
-const createDjinnConfig = require('./lib/write-files/createDjinnConfig');
-const createMetadata = require('./lib/write-files/createMetadata');
-const createGenerateFilesConfig = require('./lib/write-files/createGenerateFilesConfig');
-const createGeneratedFiles = require('./lib/write-files/createGeneratedFiles');
+const createDjinnConfig = require('./lib/write-files/djinn-config/createDjinnConfig');
+const createMetadata = require('./lib/write-files/metadata/createMetadata');
+const createGenerateFilesConfig = require('./lib/write-files/generate-files/createGenerateFilesConfig');
+const createGeneratedFiles = require('./lib/write-files/generate-files/createGeneratedFiles');
 
 const main = async () => {
     try {
