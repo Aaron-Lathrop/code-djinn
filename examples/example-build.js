2808c2506693f8215cb1d0f0a396f41d2774679a
Aaron Lathrop <alathrop78@gmail.com>
2020-11-27 06:44:27 -0700

    add requirement to include params in buildSteps definition for moduleExports, improve template formatting, code clean up

--- examples/example-build.js
+++ examples/example-build.js
@@ -1,4 +1,4 @@
-const buildPaths = ['dist/dataservices', 'dist/models', 'dist/repositories'];
+const moduleExportPaths = ['dist/dataservices', 'dist/models', 'dist/repositories'];
 const { buildContexts } = require("./example-buildContexts");
 const djinn = require('../bin/djinn');
 const builder = djinn();
@@ -6,15 +6,14 @@ const { buildFile, buildNewFiles, buildDirectoryModuleExports } = builder;
 
 builder.build({
     contexts: buildContexts,
-    paths: buildPaths,
     templateDir: 'example-templates',
     outputDir : 'dist',
     options: {
-        rewriteAll: false
+        rewriteAll: true
     },
     buildSteps: [
         buildNewFiles(),
-        buildDirectoryModuleExports(),
+        buildDirectoryModuleExports(moduleExportPaths, 'index.js', false),
         buildFile('template.App.txt', 'dist', {
             rewritable: true
         })
